3bc0213d7fc70da2222a89577df813627cf86e82
julian <nguyenchivuong6@gmail.com>
2022-05-26 14:04:28 +0700

    Creat router, app/controller folder

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 const path = require('path');
-const express = require('express')
-const morgan = require('morgan')
-const handlebars = require('express-handlebars')
-const app = express()
-const port = 3000
+const express = require('express');
+const morgan = require('morgan');
+const handlebars = require('express-handlebars');
+const app = express();
+const port = 3000;
 
+const route = require('./routes');
 
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(express.urlencoded({
+  extended: true
+}));
+
+app.use(express.json());
 //HTTP logger
 app.use(morgan('combined'))
 
@@ -18,16 +25,11 @@ app.set('view engine', 'hbs')
 app.set('views',path.join(__dirname, 'resources', 'views'))
 
 
-app.get('/', (req, res) => {
-  res.render('home');
-})
+//Routes init
+route(app);
 
-app.get('/news', (req, res) => {
-  res.render('news');
-})
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
-
-
